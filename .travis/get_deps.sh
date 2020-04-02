fae7b1c2359560b578b33191afc57a698f02e02f
rhubard <18734141014@163.com>
2020-04-02 18:21:32 +0800

    test

--- .travis/get_deps.sh
+++ .travis/get_deps.sh
@@ -4,6 +4,6 @@ HUGO_FILENAME="hugo_${HUGO_RELEASE}_Linux-64bit"
 HUGO_TARFILE="${HUGO_FILENAME}.tar.gz"
 
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_RELEASE/$HUGO_TARFILE
-tar zxvf $HUGO_TARFILE
+tar zxf $HUGO_TARFILE
 mv $HUGO_FILENAME/hugo ./hugo
 rm -rf $HUGO_FILENAME
